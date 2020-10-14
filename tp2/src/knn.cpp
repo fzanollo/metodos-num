5c2bb6ee6566bfdb9d24e9b7a3391e7cdd25952c
Luis García Gómez <garciagomezluis.94@gmail.com>
2020-10-14 03:56:31 -0300

    agrego knn

--- tp2/src/knn.cpp
+++ tp2/src/knn.cpp
@@ -1,29 +1,48 @@
-#include <algorithm>
-//#include <chrono>
-#include <iostream>
 #include "knn.h"
 
-using namespace std;
-
-
-KNNClassifier::KNNClassifier(unsigned int n_neighbors)
-{
+KNNClassifier::KNNClassifier(uint n_neighbors) {
+    this->n_neighbors = n_neighbors;
 }
 
-void KNNClassifier::fit(Matrix X, Matrix y)
-{
+void KNNClassifier::fit(Matrix X, Matrix y) {
+    this->X = X; this->y = y;
 }
 
-
-Vector KNNClassifier::predict(Matrix X)
-{
-    // Creamos vector columna a devolver
+Vector KNNClassifier::predict(Matrix X) {
     auto ret = Vector(X.rows());
 
-    for (unsigned k = 0; k < X.rows(); ++k)
-    {
-        ret(k) = 0;
+    for (uint k = 0; k < X.rows(); ++k) {
+        ret(k) = predictOne((Vector) X.row(k), KNNClassifier::electionMode);
+        if(k % 100 == 0) cout << "Listo " << k << "/" << X.rows() << " del set de validación" << endl; // <-- comentar si molesta
+    }
+    return ret;
+}
+
+uint KNNClassifier::predictOne(Vector x, KNNClassifier::election_strategy_fn election) {
+    // n elementos ordenados por distancia
+    point_queue queue(point_cmp);
+
+    // comparaciones
+    for(uint k = 0; k < X.rows(); k++) {
+        double distance = (x.transpose() - X.row(k)).norm();
+        uint label = y(k, 0);
+        queue.push(point(distance, label));
+    }
+    return election(queue, n_neighbors);
+}
+
+uint KNNClassifier::electionMode(point_queue queue, uint n_neighbors) {
+    const uint n_symbols = 10;
+    uint votes[n_symbols] = {0};
+
+    for(uint i = 0; i < n_neighbors; i++) {
+        votes[queue.top().second]++;
+        queue.pop();
     }
 
-    return ret;
+    uint label = 0;
+    for(uint i = 0; i < n_symbols; i++) {
+        label = votes[i] >= votes[label] ? i : label;
+    }
+    return label;
 }
--- tp2/src/knn.h
+++ tp2/src/knn.h
@@ -1,14 +1,39 @@
 #pragma once
 
+#include <algorithm>
+//#include <chrono>
+#include <iostream>
+#include <utility>
+#include <queue>
+#include <functional>
 #include "types.h"
 
+using namespace std;
 
 class KNNClassifier {
 public:
-    KNNClassifier(unsigned int n_neighbors);
-
+    KNNClassifier(uint n_neighbors);
     void fit(Matrix X, Matrix y);
-
     Vector predict(Matrix X);
 private:
+    // point = par (distancia, label)
+    typedef pair<double, uint> point;
+
+    // comparador de point por distancia
+    function<bool(point, point)> point_cmp = [](point left, point right) { return left.first > right.first; };
+
+    // min queue para point (rápido para sacar el más cercano)
+    typedef priority_queue<point, vector<point>, decltype(point_cmp)> point_queue;
+
+    typedef function<uint(point_queue, uint)> election_strategy_fn;
+
+    uint n_neighbors;
+    Matrix X;
+    Matrix y;
+
+    // Predicción sólo para un elemento. "predict" lo hace para todas las rows de la matriz entrante.
+    uint predictOne(Vector, election_strategy_fn);
+
+    // Algoritmo de selección de label en base a la moda.
+    static uint electionMode(point_queue, uint);
 };
