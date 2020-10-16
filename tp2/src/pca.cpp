5aade13739144eb065ce4bc8a0e381f07e180f18
fzanollo <fzanollo@dc.uba.ar>
2020-10-16 11:38:54 -0300

    pca ok

--- tp2/src/pca.cpp
+++ tp2/src/pca.cpp
@@ -14,28 +14,19 @@ void PCA::fit(Matrix X)
 	//es col y no row porque eigen toma vectores verticales
 	//cambiar por rowwise de ser necesario
 	uint n = X.rows();
-	uint m = X.cols();
+	// uint m = X.cols();
 
-	Vector mu = (X.colwise().sum()) / n;
+    Vector mu = X.colwise().mean();
+	// Vector mu = (X.colwise().sum()) / n;
 	cout << endl<< "mu " << mu << endl;
 
-	// intento de hacerlo a mano, da mal
-	// Matrix Xmu = Matrix(n,m);
-	// for(auto row : X.transpose().colwise()){
-	// 	cout << "otra: ";
-	// 	cout << row << endl;
-	// 	cout << (row - mu)/ sqrt(n-1) << endl;
- //    	Xmu << (row - mu)/ sqrt(n-1);
-	// }
-
-	// esta es la linea donde creo que esta el problema, use transpose porque eigen no te deja si pedis rowwise 
-	Matrix Xmu = (X.transpose().colwise() - mu) / sqrt(n-1);
+	Matrix Xmu = (X.rowwise() - mu.transpose()) / sqrt(n-1);
 	cout << "Xmu " << Xmu << endl;
 	
 	Matrix Mcov = Xmu.transpose() * Xmu; // X.T * X 'o' X * X.T ???
 	cout << "Mcov " << Mcov << endl;
 
-	tc = get<0>(get_first_eigenvalues(Mcov, n_components)).transpose();
+	tc = (get_first_eigenvalues(Mcov, n_components)).second;
 	cout << "tc " << tc;
 }
 
