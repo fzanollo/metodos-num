592ad0dc62e7451963f6a83ac630c393064391f1
fschlottmann <fschlottmann@dc.uba.ar>
2020-10-15 21:25:35 -0300

    Agrego main

--- tp2/src/main.cpp
+++ tp2/src/main.cpp
@@ -3,12 +3,59 @@
 //
 
 #include <iostream>
+#include "knn.h"
 #include "pca.h"
-#include "eigen.h"
+#include "data_handler.h"
 
 int main(int argc, char** argv){
+	
+	if (argc < 8) {
+		std::cerr << "Error: parametros insuficientes." << std::endl;
+		return 1;
+	}
 
-  std::cout << "Hola mundo!" << std::endl;
+	uint k_nearest = 1;
+	uint n_pca = 784;
 
-  return 0;
+	char method = *argv[2];
+	char* train_path = argv[4];
+	char* test_path = argv[6];
+	char* out_path = argv[8];
+
+	DataHandler dh(train_path, test_path);
+
+	KNNClassifier kNN(k_nearest);
+	
+	switch (method) {
+
+		case '0' :
+			/* kNN */
+			kNN.fit(dh.train_data(), dh.train_label());
+
+			dh.classif() = kNN.predict(dh.test_data());
+			break;
+
+		case '1' :
+			/* PCA + kNN */
+			PCA pca(n_pca);
+			pca.fit(dh.train_data());
+
+			dh.train_data() = pca.transform(dh.train_data());
+			dh.test_data() = pca.transform(dh.test_data());
+
+			kNN.fit(dh.train_data(), dh.train_label());
+
+			dh.classif() = kNN.predict(dh.test_data());
+			break;
+
+	}
+
+	dh.export_classif(out_path);
+
+	std::cout << "# Vecino mas cercanos: " << k_nearest << std::endl;
+	if (method == '1')
+		std::cout << "# Components principales: " << n_pca << std::endl;
+	std::cout << "Tasta de reconocimiento: " << std::endl;
+
+  	return 0;
 }
