a2c5dfe10cda69c51d8442b681bc98554e8b5a39
gianluca_capelo <gianluca.capelo@gmail.com>
2020-11-28 16:26:12 -0300

    agrego solver

--- tp3/src/linear_regression.cpp
+++ tp3/src/linear_regression.cpp
@@ -10,16 +10,23 @@ namespace py=pybind11;
 
 LinearRegression::LinearRegression()
 {
+    
 }
 
 void LinearRegression::fit(Matrix X, Matrix y)
 {
+    //Quiero resolver el sistemas (X^t)X(solucion) = (X^t)y 
+    Matrix A = X.transpose() * X;
+    Vector b = X.transpose() * y;
+    Vector x = A.householderQr().solve(b);
+    this->coef = x;
+
 }
 
 
 Matrix LinearRegression::predict(Matrix X)
 {
-    auto ret = MatrixXd::Zero(X.rows(), 1);
-
-    return ret;
+    //auto ret = MatrixXd::Zero(X.rows(), 1);
+    //return ret;
+    return X * this->coef;
 }
